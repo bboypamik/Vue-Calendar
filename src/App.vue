de00667bb9c94fedaf9af46f39a8d194167d3262
Miroslav Pantos <bboypamik@gmail.com>
2020-11-11 23:34:12 +0100

    Vue-calendar css && bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+<calendar />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import calendar from "./components/calendar";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    calendar
   }
 }
 </script>
@@ -19,7 +19,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/calendar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="calendar container">
+        <h1>Calendar</h1>
+        <div  class="pt-5  calendar__top-bar">
+            <p class="d-inline-flex font-weight-bold" v-for="day in state.days" :key="day">{{day}}</p>
+        </div>
+        <div class="calendar__numbers">
+            <p class="d-inline-flex" v-for="num in 30" :key="num">{{num}}</p>
+        </div>
+
+        <button class="btn btn-primary"> Prev</button>
+        <button class="btn btn-primary m-2">Next</button>
+</div>
+</template>
+
+<script>
+    import {reactive} from 'vue';
+    export default {
+        name: "calendar",
+
+        setup(){
+
+            const state = reactive({
+
+                days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+            })
+
+            return {
+                state
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .calendar__top-bar p {
+        width: 14.28%;
+    }
+
+    .calendar__numbers p {
+        width: 14.28%;
+    }
+
+</style>
